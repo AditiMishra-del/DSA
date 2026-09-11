bb53d8ade1e31dd3c3c320c0aa84b56a022592cf
Aditi Mishra <aditimishra9555@gmail.com>
2026-09-11 14:49:31 +0000

    5

---
+++ basics.cpp/5.cpp
@@ -0,0 +1,17 @@
+// divisors
+#include <bits/stdc++.h>
+using namespace std;
+void divisor(int n){
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<" ";
+        }
+    }
+}
+
+int main() {
+   int cnt,n;
+   cin>>n;
+   divisor(n);
+ 
+}
