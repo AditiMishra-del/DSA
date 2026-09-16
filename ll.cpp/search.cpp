829dc5aed4d2859f42a36456bfc70bbb6f5f60d7
Aditi Mishra <aditimishra9555@gmail.com>
2026-09-16 17:48:42 +0000

    3

---
+++ ll.cpp/search.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+public:
+    int data;
+    node* next;
+    node(int data1,node* next1){
+        data=data1;
+        next=next1;
+    }
+    node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+node* convert2ll(vector<int> &arr){
+    node* head= new node(arr[0]);
+    node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        node* temp=new node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+}
+int lengthofll(node* head){
+    int cnt=0;
+    node* temp=head;
+    while(temp){
+        temp=temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+int checkifpresent(node* head,int val){
+    node* temp=head;
+    while(temp){
+        if(temp->data==val) return 1;
+        temp=temp->next;
+    }
+    return 0;
+}
+int main() {
+	vector<int>arr={12,5,7,6};
+	node* head=convert2ll(arr);
+	cout<<checkifpresent(head,15);
+}
